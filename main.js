c3da8775e44eb2c5d9becc4ed11fa413da0dfb5e
arshpreet7 <89577932+arshpreet7@users.noreply.github.com>
2022-06-22 21:26:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+Arrowkey="Arrkey.png"
+alpakey="Alpkey.png"
+Numberkey="numberkey"
+other_key="otherkey.png"
+spelkey= "spkey.png"
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+//Write a code to grab the key-pressed event
+window.addEventListener("keydown" , my_keydown);
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		console.log("you have pressed an alphabet key");
+		aplhabetkey()
+
+	else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+}
+
+function aplhabetkey()
+{
+	img_image="Alpkey.png";
+	add();
+}//upload respective image with the message. 
+
+
+function numberkey()
+{
+	img_image="numberkey.png";
+	add();
+}
+function arrowkey()
+{
+	img_image="Arrkey.png";
+	add();
+}
+function specialkey()
+{
+    img_image="spkey.png";
+	add();	
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
+keyPressed=e.keyCode;
+console.log(keyPressed);
+if (e.shiftKey==true && keyPressed =="80")
+{
+	console.log("p and shift key pressed together");
+	block_image_width = block_image_width + 10;
+	block_image_height = block_image_height + 10;
+	document.getElementById("current_width").innerHTML=block_image_width;
+	document.getElementById("current_height").innerHTML=block_image_height;
+}
+if (e.shiftKey==true && keyPressed =="77")
+{
+	console.log("m and shift key pressed together");
+	block_image_width = block_image_width - 10;
+	block_image_height = block_image_height - 10;
+	document.getElementById("current_width").innerHTML=block_image_width;
+	document.getElementById("current_height").innerHTML=block_image_height;
+}
